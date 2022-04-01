85686a8a34f76a5657bf3d28f94ea53117204e8e
Zane Bliss <zane.bliss@daveramsey.com>
2022-04-01 16:51:03 -0500

    wip

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import ThemeContext from "./ThemeContext";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
 
@@ -10,6 +11,7 @@ const SearchInput = () => {
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     requestPets();
@@ -75,6 +77,20 @@ const SearchInput = () => {
             ))}
           </select>
         </label>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="darkblue">Dark Blue</option>
+            <option value="peru">Peru</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
 import ReactDOM from "react-dom";
-import { StrictMode } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import SearchInput from "./SearchParams";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { useState, StrictMode } from "react";
+import SearchInput from "./SearchInput";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Router>
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route path="/">
-            <SearchInput />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/home">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchInput />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
